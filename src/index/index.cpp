19ecba82b04d9fdfb9b7b1673da46c28dd8a39be
littleblus <littleblus@163.com>
2024-02-07 01:16:01 +0800

    构建正排索引

--- src/index/index.cpp
+++ src/index/index.cpp
@@ -1,1 +1,70 @@
 #include "index.h"
+#include <iostream>
+
+#define SEP '\3'
+
+namespace index {
+	// struct doc_info {
+	// 	std::string url;
+	// 	std::string title;
+	// 	std::string content;
+	// 	uint64_t doc_id;
+	// };
+
+	// struct inverted_elem {
+	// 	std::string word;
+	// 	uint64_t doc_id;
+	// 	int weight;
+	// };
+
+	void Index::build(std::string raw_path) {
+		std::fstream file(raw_path, std::ios::in | std::ios::binary);
+		if (!file.is_open()) {
+			throw std::runtime_error("build file open failed");
+		}
+		std::string line;
+		int errcnt = 0;
+		while (std::getline(file, line)) {
+			// 建立正排索引
+			doc_info doc;
+			doc.doc_id = forward_index.size();
+			// url\3title\3content
+			size_t url_end = line.find(SEP);
+			if (url_end == std::string::npos) {
+				std::cerr << "build file format error, count:" << errcnt << '\n';
+				continue;
+			}
+			size_t title_end = line.find(SEP, url_end + 1);
+			if (title_end == std::string::npos) {
+				std::cerr << "build file format error, count:" << errcnt << '\n';
+				continue;
+			}
+			doc.url = line.substr(0, url_end);
+			doc.title = line.substr(url_end + 1, title_end - url_end - 1);
+			doc.content = line.substr(title_end + 1);
+			// 建立倒排索引
+
+			forward_index.push_back(std::move(doc));
+		}
+	}
+
+	const index::doc_info& Index::get_doc_info(uint64_t doc_id) const {
+		if (doc_id >= forward_index.size() || doc_id < 0) {
+			throw std::out_of_range("doc_id out of range");
+		}
+		return forward_index[doc_id];
+	}
+
+	index::doc_info& Index::get_doc_info(uint64_t doc_id) {
+		return const_cast<index::doc_info&>(static_cast<const index::Index&>(*this).get_doc_info(doc_id));
+	}
+
+	const std::vector<inverted_elem>& Index::get_inverted_index(const std::string& word) const {
+		auto it = inverted_index.find(word);
+		return it == inverted_index.end() ? std::vector<inverted_elem>() : it->second;
+	}
+
+	std::vector<inverted_elem>& Index::get_inverted_index(const std::string& word) {
+		return const_cast<std::vector<inverted_elem>&>(static_cast<const index::Index&>(*this).get_inverted_index(word));
+	}
+}
--- src/index/index.h
+++ src/index/index.h
@@ -2,38 +2,35 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <fstream>
 
 namespace index {
-    struct doc_info {
-        std::string url;
-        std::string title;
-        std::string content;
-        uint64_t doc_id;
-    };
+	struct doc_info {
+		std::string url;
+		std::string title;
+		std::string content;
+		uint64_t doc_id;
+	};
 
-    struct inverted_elem {
-        std::string word;
-        uint64_t doc_id;
-        int weight;
-    };
+	struct inverted_elem {
+		uint64_t doc_id;
+		int weight;
+	};
 
+	class Index {
+	public:
+		// 构建索引
+		void build(std::string raw); // 传入的是文档的处理后内容
 
-    class Index {
-    public:
-        Index();
+		// 获取正排索引
+		const doc_info& get_doc_info(uint64_t doc_id) const;
+		doc_info& get_doc_info(uint64_t doc_id);
 
-        // 构建索引
-        void build(std::string raw); // 传入的是文档的处理后内容
-
-        // 获取正排索引
-        const doc_info& get_doc_info(uint64_t doc_id) const;
-        doc_info& get_doc_info(uint64_t doc_id);
-
-        // 获取倒排索引
-        const auto& get_inverted_index() const;
-        auto& get_inverted_index();
-    private:
-        std::vector<doc_info> forward_index; // 正排索引，文档id对应下标
-        std::unordered_map<std::string, std::vector<inverted_elem>> inverted_index; // 倒排索引
-    };
+		// 获取倒排索引
+		const std::vector<inverted_elem>& get_inverted_index(const std::string& word) const;
+		std::vector<inverted_elem>& get_inverted_index(const std::string& word);
+	private:
+		std::vector<doc_info> forward_index; // 正排索引，文档id对应下标
+		std::unordered_map<std::string, std::vector<inverted_elem>> inverted_index; // 倒排索引
+	};
 }
